d9f5c33e4668fd52a2c7d7f535053ea8e8e304cb
Алижуха <manasbayev00@gmail.com>
2026-09-24 09:29:41 +0600

    бла бла

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n=s.nextInt();
-        System.out.println("The next number for the number " + n + " is " + (n + 1) + ".");
-        System.out.println("The previous number for the number " + n + " is " + (n - 1) + ".");
+        Scanner pisya=new Scanner(System.in);
+        int N=pisya.nextInt();
+        int K=pisya.nextInt();
+        System.out.println(K / N);
+    }
+}
